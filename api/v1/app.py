949325b99e63c80f820d21ca0b272e1be561035d
Ken-wangari <wangariken50@gmail.com>
2024-05-01 05:33:14 -0700

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,17 +8,18 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from flasgger import Swagger
+from flasgger.utils import swag_from
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown(self):
     """Removes the current SQLAlchemy Session"""
-    storage.close()
+    return storage.close()
 
 
 @app.errorhandler(404)
@@ -36,7 +37,6 @@ Swagger(app)
 
 
 if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
-
