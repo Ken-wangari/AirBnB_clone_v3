95588f03e6fe40b6eb3a16f687e33e78b1ba7be3
Ken-wangari <wangariken50@gmail.com>
2024-05-01 04:00:05 -0700

    AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,48 @@
-#!/usr/bin/python3xx
-'''api status'''
+#!/usr/bin/python3
+"""
+This module implements a Flask web application for an AirBnB clone RESTful API
+"""
+from flask import Flask, jsonify
 import models
-from models import storage
-from models.base_model import BaseModel
-from flask import jsonify
 from api.v1.views import app_views
 
-
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return stuff'''
-    return jsonify(status='OK')
+app = Flask(__name__)
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def view_status():
+    """Returns the status of the application"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def view_stats():
+    """Retrieves the number of each object by type"""
+    model_counts = {
+        "amenities": models.storage.count(models.Amenity),
+        "cities": models.storage.count(models.City),
+        "places": models.storage.count(models.Place),
+        "reviews": models.storage.count(models.Review),
+        "states": models.storage.count(models.State),
+        "users": models.storage.count(models.User)
+    }
+    return jsonify(model_counts)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Closes the current SQLAlchemy Session"""
+    models.storage.close()
+
+
+@app.errorhandler(404)
+def handle_404_error(error):
+    """Handles 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == '__main__':
+    host = "0.0.0.0"
+    port = 5000
+    app.run(host=host, port=port, threaded=True)
+
