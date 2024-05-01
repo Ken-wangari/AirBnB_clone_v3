cc45043474ef39e379405fb902501e37f535d914
Ken-wangari <wangariken50@gmail.com>
2024-05-01 03:49:19 -0700

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
 """
-    This script starts a Flask web application
+This script starts a Flask web application for an AirBnB clone RESTful API
 """
 from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
 from flasgger import Swagger
 from flasgger.utils import swag_from
+from models import storage
+from api.v1.views import app_views
 
-
+# Initialize Flask application
 app = Flask(__name__)
+
+# Register blueprint
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
+# Enable CORS for all routes under /api/v1
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
-@app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
-    return storage.close()
-
-
-@app.errorhandler(404)
-def error(e):
-    """Handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
-
-
+# Define Swagger configuration
 app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
+    'title': 'AirBnB Clone RESTful API',
     'uiversion': 3
 }
 
+# Initialize Swagger
 Swagger(app)
 
 
+# Define teardown function to close SQLAlchemy Session
+@app.teardown_appcontext
+def teardown(error):
+    """Closes the current SQLAlchemy Session"""
+    storage.close()
+
+
+# Define error handler for 404 errors
+@app.errorhandler(404)
+def handle_404_error(error):
+    """Handles 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
+    # Retrieve host and port from environment variables or use defaults
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+
+    # Run the Flask application
     app.run(host=host, port=port, threaded=True)
+
