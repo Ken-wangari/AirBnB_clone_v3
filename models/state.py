d152e7378e1f018513d2619495da2ea7d97a6dc9
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-10 15:14:24 -0700

    state

--- models/state.py
+++ models/state.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python3
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-"""Module for the State class."""
 
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """State class to represent state information."""
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City",
+            cascade='all, delete, delete-orphan',
+            backref="state"
+        )
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initialize State class."""
+        """initializes state"""
         super().__init__(*args, **kwargs)
-        self.name = ""
 
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            c_list = []
+            a_cities = models.storage.all(City)
+            for city in a_cities.values():
+                if city.state_id == self.id:
+                    c_list.append(city)
+            return c_list
