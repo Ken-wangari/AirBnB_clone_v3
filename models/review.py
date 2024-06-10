c524ab9588d427a9e2ef955763b7796e3a0b9076
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-10 15:22:37 -0700

    reviewed

--- models/review.py
+++ models/review.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+""" holds class Review"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
-"""Module for the Review class."""
 
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """Review class to represent review information."""
+class Review(BaseModel, Base):
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
 
     def __init__(self, *args, **kwargs):
-        """Initialize Review class."""
+        """initializes Review"""
         super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
-
