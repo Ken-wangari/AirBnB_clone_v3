c78a53cea5dea61e25f39ed1c85710dac2e3e479
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-10 15:19:08 -0700

    city

--- models/city.py
+++ models/city.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+""" holds class City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-"""Module for the City class."""
 
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """City class to represent city information."""
+class City(BaseModel, Base):
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship(
+            "Place",
+            cascade='all, delete, delete-orphan',
+            backref="cities"
+        )
+    else:
+        state_id = ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initialize City class."""
+        """initializes city"""
         super().__init__(*args, **kwargs)
-        self.state_id = ""
-        self.name = ""
-
