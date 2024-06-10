46c633c8bf22d3fa2313188f03491eb268fc8ac8
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-06-10 15:17:33 -0700

    base place

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,83 @@
 #!/usr/bin/python3
-
-"""Module for the BaseModel class and utilities"""
-
+"""
+Contains class BaseModel
+"""
+import inspect
 from datetime import datetime
-from models import storage
-from uuid import uuid4 as uuid
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
-    """BaseModel class to serve as the foundation for other models"""
+    """The BaseModel class from which future classes will be derived"""
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Initialize a BaseModel instance"""
+        """Initialization of the base model"""
         if kwargs:
-            self.set_by_kwargs(**kwargs)
+            for k, val in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, val)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
-            self.id = str(uuid())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-
-    @staticmethod
-    def isoparse(isoformat):
-        """Parse a date from ISO format to datetime object"""
-        return datetime.strptime(isoformat, "%Y-%m-%dT%H:%M:%S.%f")
-
-    def set_by_kwargs(self, **kwargs):
-        """Set properties by kwargs to a BaseModel instance"""
-        for k, v in kwargs.items():
-            if k == 'created_at' or k == 'updated_at':
-                setattr(self, k, self.isoparse(v))
-            elif k != '__class__':
-                setattr(self, k, v)
-
-    def save(self):
-        """Save changes of a BaseModel instance and update JSON file"""
-        self.updated_at = datetime.now()
-        storage.save()
-
-    def to_dict(self):
-        """Return all info of a BaseModel instance as a dictionary"""
-        return {
-            **self.__dict__,
-            '__class__': self.__class__.__name__,
-            'updated_at': self.updated_at.isoformat(),
-            'created_at': self.created_at.isoformat()
-        }
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """Print info of a BaseModel instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
+    def save(self):
+        """updates the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of the instance"""
+        n_dict = self.__dict__.copy()
+        if "created_at" in n_dict:
+            n_dict["created_at"] = n_dict["created_at"].strftime(time)
+        if "updated_at" in n_dict:
+            n_dict["updated_at"] = n_dict["updated_at"].strftime(time)
+        n_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in n_dict:
+            del n_dict["_sa_instance_state"]
+        frame = inspect.currentframe().f_back
+        funct_name = frame.f_code.co_name
+        class_name = ''
+        if 'self' in frame.f_locals:
+            class_name = frame.f_locals["self"].__class__.__name__
+        is_fs_writing = funct_name == 'save' and class_name == 'FileStorage'
+        if 'password' in n_dict and not is_fs_writing:
+            del n_dict['password']
+        return n_dict
+
+    def delete(self):
+        """delete the current instance from the storage"""
+        models.storage.delete(self)
--- models/place.py
+++ models/place.py
@@ -1,25 +1,98 @@
 #!/usr/bin/python3
+""" holds class Place"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
-"""Module for the Place class."""
+if models.storage_t == 'db':
+    place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            nullable=False,
+            primary_key=True
+        ),
+        Column(
+            'amenity_id',
+            String(60),
+            ForeignKey('amenities.id'),
+            nullable=False,
+            primary_key=True
+        )
+    )
 
-from models.base_model import BaseModel
 
-
-class Place(BaseModel):
-    """Place class to represent place information."""
+class Place(BaseModel, Base):
+    """Representation of Place """
+    if models.storage_t == 'db':
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship(
+            "Review",
+            cascade="all, delete, delete-orphan",
+            backref="place"
+        )
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 backref="place_amenities",
+                                 viewonly=False)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
 
     def __init__(self, *args, **kwargs):
-        """Initialize Place class."""
+        """initializes Place"""
         super().__init__(*args, **kwargs)
-        self.city_id = ""
-        self.user_id = ""
-        self.name = ""
-        self.description = ""
-        self.number_rooms = 0
-        self.number_bathrooms = 0
-        self.max_guest = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
 
+    if models.storage_t != 'db':
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            from models.review import Review
+            rev_list = []
+            all_revs = models.storage.all(Review)
+            for review in all_revs.values():
+                if review.place_id == self.id:
+                    rev_list.append(review)
+            return rev_list
+
+        @property
+        def amenities(self):
+            """getter attribute returns the list of Amenity instances"""
+            from models.amenity import Amenity
+            amen_of_place = []
+            for value in models.storage.all(Amenity).values():
+                if value.id in self.amenity_ids:
+                    amen_of_place.append(value)
+            return amen_of_place
+
+        @amenities.setter
+        def amenities(self, value):
+            """Adds an amenity to this Place"""
+            from models.amenity import Amenity
+            if type(value) is Amenity:
+                if value.id not in self.amenity_ids:
+                    self.amenity_ids.append(value.id)
